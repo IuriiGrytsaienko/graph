6d86327d79ea021cee627c691dbd822d53666b1a
Leonid <leonid.baida@gmail.com>
2016-10-23 13:57:55 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function average(graph) {
+    //
+}
+
+function min(graph) {
+    //
+}
+
+function max(graph) {
+    //
+}
+
+console.log(average(graph));
+console.log(min(graph));
+console.log(max(graph));
